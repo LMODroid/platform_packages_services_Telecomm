8fee6ad11c14452d6ba00e634b3bc6d015f09e59
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:35:10 +0800

    Incall vibration options [2/3]
    *allow setting vibration when call is connected
    *allow setting vibration when call is disconnected
    *allow setting vibration for call waiting
    *this works with google and aosp dialer :)
    
        From: nift4 <nift4@protonmail.com>
        Date: Sat, 7 Oct 2023 20:21:41 +0200
        Subject: [PATCH] Telecomm: fix incall vib
    
        Change-Id: If9340295211c8be48afa68aae1abd46344779d41
    
    Change-Id: I1dcdf20d022df94cf798548f0c3e2d524f8b21ca
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- src/com/android/server/telecom/InCallController.java
+++ src/com/android/server/telecom/InCallController.java
@@ -47,9 +47,12 @@ import android.os.PackageTagsList;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.os.Vibrator;
+import android.provider.Settings;
 import android.telecom.CallAudioState;
 import android.telecom.CallEndpoint;
 import android.telecom.ConnectionService;
+import android.telecom.DisconnectCause;
 import android.telecom.InCallService;
 import android.telecom.Log;
 import android.telecom.Logging.Runnable;
@@ -68,6 +71,8 @@ import com.android.internal.util.IndentingPrintWriter;
 import com.android.server.telecom.SystemStateHelper.SystemStateListener;
 import com.android.server.telecom.flags.FeatureFlags;
 import com.android.server.telecom.ui.NotificationChannelManager;
+
+import com.libremobileos.providers.LMOSettings;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -1689,7 +1694,25 @@ public class InCallController extends CallsManagerListenerBase implements
         Log.i(this, "onCallStateChanged: Call state changed for TC@%s: %s -> %s", call.getId(),
                 CallState.toString(oldState), CallState.toString(newState));
         maybeTrackMicrophoneUse(isMuted());
+        boolean vibrateOnConnect = Settings.System.getIntForUser(mContext.getContentResolver(),
+            LMOSettings.System.VIBRATE_ON_CONNECT, 0, UserHandle.USER_CURRENT) >= 1;
+        boolean vibrateOnDisconnect = Settings.System.getIntForUser(mContext.getContentResolver(),
+            LMOSettings.System.VIBRATE_ON_DISCONNECT, 0, UserHandle.USER_CURRENT) >= 1;
+
+        if (oldState == CallState.DIALING && newState == CallState.ACTIVE && vibrateOnConnect) {
+            vibrate(100, 200, 0);
+        } else if (oldState == CallState.ACTIVE && newState == CallState.DISCONNECTED
+                && vibrateOnDisconnect) {
+            vibrate(100, 200, 0);
+        }
         updateCall(call);
+    }
+
+    public void vibrate(int v1, int p1, int v2) {
+        long[] pattern = new long[] {
+            0, v1, p1, v2
+        };
+        ((Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(pattern, -1);
     }
 
     @Override
