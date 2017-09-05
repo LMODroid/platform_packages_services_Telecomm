88ef1a13b87ad996481cd095d929c5a4314c85b3
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:35:10 +0800

    Incall vibration options [2/3]
    *allow setting vibration when call is connected
    *allow setting vibration when call is disconnected
    *allow setting vibration for call waiting
    *this works with google and aosp dialer :)
    
        From: nift4 <nift4@protonmail.com>
        Date: Sat, 7 Oct 2023 20:21:41 +0200
        Subject: [PATCH] Telecomm: fix incall vib
    
        Change-Id: If9340295211c8be48afa68aae1abd46344779d41
    
    Change-Id: I1dcdf20d022df94cf798548f0c3e2d524f8b21ca
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -46,7 +46,9 @@ import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.os.vibrator.persistence.ParsedVibration;
 import android.os.vibrator.persistence.VibrationXmlParser;
+import android.provider.Settings;
 import android.telecom.Log;
+import android.telecom.DisconnectCause;
 import android.telecom.TelecomManager;
 import android.util.Pair;
 import android.view.accessibility.AccessibilityManager;
@@ -54,6 +56,8 @@ import android.view.accessibility.AccessibilityManager;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.server.telecom.LogUtils.EventTimer;
 import com.android.server.telecom.flags.FeatureFlags;
+
+import com.libremobileos.providers.LMOSettings;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -642,6 +646,11 @@ public class Ringer {
 
         stopRinging();
 
+        if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                LMOSettings.System.VIBRATE_ON_CALLWAITING, 0, UserHandle.USER_CURRENT) >= 1) {
+            vibrate(200, 300, 500);
+        }
+
         if (mCallWaitingPlayer == null) {
             Log.addEvent(call, LogUtils.Events.START_CALL_WAITING_TONE, reason);
             mCallWaitingCall = call;
@@ -879,6 +888,13 @@ public class Ringer {
             mHandler = handlerThread.getThreadHandler();
         }
         return mHandler;
+    }
+
+    public void vibrate(int v1, int p1, int v2) {
+        long[] pattern = new long[] {
+            0, v1, p1, v2
+        };
+        ((Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(pattern, -1);
     }
 
     @VisibleForTesting
