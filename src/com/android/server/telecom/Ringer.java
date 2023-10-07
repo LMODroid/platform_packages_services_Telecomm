dfaf1befa12194a5ac6a08a38851b51b25acb894
nift4 <nift4@protonmail.com>
2023-10-07 20:21:41 +0200

    Telecomm: fix incall vib
    Change-Id: If9340295211c8be48afa68aae1abd46344779d41

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -464,7 +464,7 @@ public class Ringer {
         stopRinging();
 
         if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.VIBRATE_ON_CALLWAITING, 0, UserHandle.USER_CURRENT) == 1) {
+                Settings.System.VIBRATE_ON_CALLWAITING, 0, UserHandle.USER_CURRENT) >= 1) {
             vibrate(200, 300, 500);
         }
 
